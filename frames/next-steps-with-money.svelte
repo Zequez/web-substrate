2cc5f010cc9382924a7b0f3fde39898efc5c11a1
Ezequiel Adrian Schwartzman <zequez@gmail.com>
2025-05-10 05:29:05 -0300

    Added substrate information

---
+++ frames/next-steps-with-money.svelte
@@ -0,0 +1,7 @@
+<div
+  class="bg-gray-200 text-black/80 rounded-b-md size-full flexcc p8 text-center text-[20px]"
+>
+  Bridgehouse Stewarding Contribution (Food / Services / Repairs / Improvements)
+  | Upcoming Rage Club | Possibility Labs Brazil in August | Worktalk | Books |
+  Personal Expenses
+</div>
---
+++ frames/web-substrate.svelte
@@ -0,0 +1,37 @@
+<div class="size-full bg-green-700 text-center text-white text-[20px] relative">
+  <img
+    class="absolute w-full h-full z-10"
+    src="/images/substrate.png"
+    alt="Moss looking substrate"
+  />
+  <div
+    class="absolute inset-0 z-20 text-shadow-[0px_0px_2px_rgba(0,0,0,0.8)] flex flex-col"
+  >
+    <div class="my7 text-[30px]">Welcome to the</div>
+    <div class="font-serif -mt8 text-[50px]">WEB SUBSTRATE</div>
+    <div class="text-2xl mb5">Gameworld</div>
+    <div class="flexcc">
+      <a
+        class="b-b-1.2 b-dashed hover:b-solid"
+        href="https://github.com/zequez/web-substrate"
+        >Github @ zequez/web-substrate</a
+      >
+    </div>
+
+    <div class="h-50 flexcc flex-col">
+      <div class="inline-block">A web creation space.</div>
+      <div class="inline-block">A living art project.</div>
+      <div class="inline-block">An experiment.</div>
+      <div class="inline-block">A digital commons.</div>
+    </div>
+  </div>
+  <div
+    class="flexcc absolute bottom-0 w-full h-20 bg-black/50 z-30 text-blue-400"
+  >
+    <a
+      class="b-b-1.2 b-color-blue-300 b-dashed hover:b-solid"
+      target="_blank"
+      href="https://t.me/+4kbz6MTscf00NzIx">Telegram @ Web Substrate Creation</a
+    >
+  </div>
+</div>
