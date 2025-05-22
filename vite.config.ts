3834eac3986193eb05b862ee21d4b3ee0186d3f1
Ezequiel Adrian Schwartzman <zequez@gmail.com>
2025-05-22 17:21:44 -0300

    Skip hot reload for the .meta.json files

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,37 @@ import UnoCSS from 'unocss/vite'
 import Icons from 'unplugin-icons/vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import AutoImport from 'unplugin-auto-import/vite'
-import path from 'path'
+import { type Plugin } from 'vite'
+
+function skipHMRPlugin(): Plugin {
+  return {
+    name: 'vite-plugin-skip-hmr',
+
+    handleHotUpdate({ file, server }) {
+      // Customize your condition here
+      if (file.endsWith('.meta.json')) {
+        console.log(`[skip-hmr] Skipping HMR for: ${file}`)
+
+        // Return empty list to skip HMR
+        return []
+      }
+
+      // Returning nothing lets Vite continue normally
+    },
+  }
+}
 
 export default defineConfig({
   server: {
     host: '0.0.0.0',
   },
-  plugins: [AutoImport({}), svelte(), UnoCSS(), Icons({ compiler: 'svelte' })],
+  plugins: [
+    AutoImport({}),
+    skipHMRPlugin(),
+    svelte(),
+    UnoCSS(),
+    Icons({ compiler: 'svelte' }),
+  ],
   resolve: {
     alias: {},
   },
